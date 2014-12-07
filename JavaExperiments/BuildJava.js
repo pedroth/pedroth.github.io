b2709947a9d5c8d5197d18f954f7ca53398d9904
pedroth <hiperpc@yahoo.com>
2014-12-07 16:41:39 +0000

    testing BuildJava

--- JavaExperiments/BuildJava.js
+++ JavaExperiments/BuildJava.js
@@ -1,3 +1,12 @@
-$.get("Test.txt", function(data) {
-    alert(data);
-};
+function LoadFile() {
+    var oFrame = document.getElementById("frmFile");
+    var strRawContents = oFrame.contentWindow.document.body.childNodes[0].innerHTML;
+    while (strRawContents.indexOf("\r") >= 0)
+        strRawContents = strRawContents.replace("\r", "");
+    var arrLines = strRawContents.split("\n");
+    alert("File " + oFrame.src + " has " + arrLines.length + " lines");
+    for (var i = 0; i < arrLines.length; i++) {
+        var curLine = arrLines[i];
+        alert("Line #" + (i + 1) + " is: '" + curLine + "'");
+    }
+}
