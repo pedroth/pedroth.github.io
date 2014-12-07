1f5780215438e55fa2473f4299a00c35c98122a3
pedroth <hiperpc@yahoo.com>
2014-12-07 16:20:05 +0000

    buildPage changes

--- JavaExperiments/BuildJava.js
+++ JavaExperiments/BuildJava.js
@@ -1,16 +1,7 @@
 function readFile() {
-    var txtFile = new XMLHttpRequest();
-    txtFile.open("GET", "Test.txt", true);
-    txtFile.onreadystatechange = function()
-    {
-      if (txtFile.readyState === 4) {  // document is ready to parse.
-        if (txtFile.status === 200) {  // file is found
-          allText = txtFile.responseText; 
-          lines = txtFile.responseText.split("\n");
-        }
-      }
-    }
-    txtFile.send(null);
+  $.get("22.txt", function(data) {
+    alert(data);
+}
 }
 
 console.log(readFile());
