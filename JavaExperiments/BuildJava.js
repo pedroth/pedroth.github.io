61a029ae03998af6029b6c1899e71ab1d39ab2f0
pedroth <hiperpc@yahoo.com>
2014-12-07 16:44:18 +0000

    testing BuildJava

--- JavaExperiments/BuildJava.js
+++ JavaExperiments/BuildJava.js
@@ -1,12 +1,28 @@
 function LoadFile() {
-    var oFrame = document.getElementById("frmFile");
-    var strRawContents = oFrame.contentWindow.document.body.childNodes[0].innerHTML;
-    while (strRawContents.indexOf("\r") >= 0)
-        strRawContents = strRawContents.replace("\r", "");
-    var arrLines = strRawContents.split("\n");
-    alert("File " + oFrame.src + " has " + arrLines.length + " lines");
-    for (var i = 0; i < arrLines.length; i++) {
-        var curLine = arrLines[i];
-        alert("Line #" + (i + 1) + " is: '" + curLine + "'");
+   var xmlhttp;
+    if (window.XMLHttpRequest)
+    {// code for IE7+, Firefox, Chrome, Opera, Safari
+         xmlhttp=new XMLHttpRequest();
+    } else {// code for IE6, IE5
+      xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    if ( xmlhttp != null ) 
+    { 
+        xmlhttp.open("GET","Test.txt",false); // the false makes this synchronous!
+        xmlhttp.send( );
+        var text = xmlhttp.responseText;
+        // text contains the ENTIRE CONTENTS of the text file 
+        // you *could* just write those contents directly to the HTML output:
+        document.write( text );
+
+        // but you might want to process that one line at a time.  if so:
+        var lines = text.split( "\n" );
+        for ( var n = 0; n < lines.length; ++n )
+        {
+            var line = lines[n];
+            // and now you can do whatever is needed with that line
+        }
     }
 }
+
+LoadFile();
