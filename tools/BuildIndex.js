1b480d17af431fa005374776c9bd8a2bbe6b0d4e
pedroth <hiperpc@yahoo.com>
2015-02-06 19:31:02 +0000

    buildindex

--- tools/BuildIndex.js
+++ tools/BuildIndex.js
@@ -33,6 +33,61 @@ function buildThumbnail(name,megaDiv,w,h) {
   megaDiv.appendChild(div);
 }
 
+function swap(v, i , j) {
+  if(i >= 0 && i < v.length && j >= 0 && j < v.length) {
+    var temp = v[i];
+    v[i] = v[j];
+    v[j] = temp;
+  }
+}
+
+function sort(v,key) {
+  var n = v.length;
+  var stack = [];
+  stack.push(0);
+  stack.push(n - 1);
+  while(stack.length > 0) {
+    var high = stack.pop();
+    var low = stack.pop();
+    /*
+    * partition
+    */
+    if(low < high) {
+      var pivot = Math.floor(n * Math.random());
+      var pvalue = key(v,pivot);
+      swap(v,pivot,high);
+      var j = 0;
+      for (var i = 0; i < high-1; i++) {
+        if(key(v,i) <= pvalue) {
+          swap(v,i,j);
+          j++;
+        }
+        swap(v,j,high);
+        stack.push(low);
+        stack.push(j-1);
+        stack.push(j+1);
+        stack.push(high);
+      }
+    }
+  }
+}
+
+function powInt(x,i) {
+  if(i === 0) {
+    return 1;
+  } else if(i === 1) {
+    return x;
+  } else {
+    var q = Math.floor(i/2);
+    var r = i % 2;
+    if(r === 0) {
+      return powInt(x * x,q);
+    } else {
+      return x * powInt(x * x,q);
+    }
+  }
+}
+
 function build() {
   var text1 = LoadFile("tools/AppletNames.txt");
   var text2 = LoadFile("tools/AppJs.txt");
@@ -49,10 +104,24 @@ function build() {
     }
   }
   
+  for (var i = 0; i < apps.length; i++) {
+    var dateStrs = apps[i][1].split("\/");
+    var acm = 0; 
+    for (var j = 0; j < dateStrs.length; j++) {
+      acm += parseFloat(dateStrs[j]) * powInt(10, 2 * i);
+    }
+    apps[i][1] = acm;
+  }
+
+  sort(apps, function key(apps,i) {
+                apps[i][1];
+            };
+  );
+
   var recentDiv = document.createElement('div');
   var h2Latest = document.createElement('h2');
   h2Latest.innerHTML = "Recent Experiments";
-  recentDiv.appendChild(h2);
+  recentDiv.appendChild(h2Latest);
   var mainSection = document.getElementById("main_content");
   mainSection.appendChild(recentDiv);
 
