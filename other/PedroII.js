a3b4ae4d3e5669a9cc02f0ac4e25794c4b1c174b
pedroth <pedrotiago92@gmail.com>
2016-06-26 17:30:40 +0100

    minor chnage

--- other/PedroII.js
+++ other/PedroII.js
@@ -19,8 +19,8 @@ var numOfFrames = 9;
 function getDataFromImage(img) {
 		var canvasAux = document.createElement('canvas');
 		var contextAux = canvasAux.getContext('2d');
-        contextAux.clearRect(0, 0, img.width, img.height);
-        contextAux.drawImage(img, 0 ,0);
+        ctx.clearRect(0, 0, img.width, img.height);
+        ctx.drawImage(img, 0 ,0);
         return contextAux.getImageData(0, 0, img.width, img.height);
 }
 
