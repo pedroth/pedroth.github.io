b6276de992c64e62aa1275b6c8e9723d9505b30a
pedroth <pedrotiago92@gmail.com>
2017-02-25 09:38:06 +0000

    update

--- JsExperiments/3DRoute/3DRoute.js
+++ JsExperiments/3DRoute/3DRoute.js
@@ -93,9 +93,9 @@ var Device = function() {
 		//this.basis[0] = [cg * ca + sg * sb * sa, cb * sa, cg * sb * sa - sg * ca];
 		//this.basis[1] = [sg * sb * ca - cg * sa, cb * ca, sg * sa + cg * sb * ca];
 		//this.basis[2] = [sg*cb, -sb, cg * cb];
-		this.basis[0] = [cg, -sg,  0];
-		this.basis[1] = [sg,  cg,  0];
-		this.basis[2] = [0,   0,   1];
+		this.basis[0] = [cg     , -sg    ,  0 ];
+		this.basis[1] = [ca * sg, ca * cg, -sa];
+		this.basis[2] = [sa * sg, sa * cg,  ca];
 	}
 }
 /**
