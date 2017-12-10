a36524d3040426be2535bca9ca0ae8329a88573d
pedroth <pedrotiago92@gmail.com>
2017-12-10 15:58:31 -0500

    Add Public chat and Eigen Simulation

--- tools/JsLib/MyCanvas/test.js
+++ tools/JsLib/MyCanvas/test.js
@@ -455,7 +455,7 @@ var giveMeLine = function(a, u) {
     return points;
 }
 
-var samples = 100;
+var samples = 10;
 
 for (var i = 0; i < samples; i++) {
     var first = randomVector(-1, 1);
@@ -463,71 +463,71 @@ for (var i = 0; i < samples; i++) {
     canvasLines.drawLine(first, second, f);
 }
 
-for (var i = 0; i < samples; i++) {
-    var first = randomVector(-3, 3);
-    var second = randomVector(-3, 3);
-    canvasLines.drawLine(first, second, g);
-}
-
-canvasLines.drawLine([0, 0], [2, 2], r);
-canvasLines.drawLine([0, 0], [-2, -2], interpolativeShader);
-
+//for (var i = 0; i < samples; i++) {
+//    var first = randomVector(-3, 3);
+//    var second = randomVector(-3, 3);
+//    canvasLines.drawLine(first, second, g);
+//}
+//
+//canvasLines.drawLine([0, 0], [2, 2], r);
+//canvasLines.drawLine([0, 0], [-2, -2], interpolativeShader);
+//
 canvasLines.paintImage();
-
-var size = canvasTriangles.getSize();
-canvasTriangles.drawLine([0, Math.floor(size[0] / 10)], [size[1], Math.floor(size[0] / 10)], r);
-canvasTriangles.drawLine([Math.floor(size[1] / 10), 0], [Math.floor(size[1] / 10), size[0]], g);
-canvasTriangles.drawLine([0, 0], [size[0]-1, size[1]-1], f);
-
-var avgTime = 0;
-for(var i = 0; i < samples; i++) {
-    var first = randomVector(0, size[0]);
-    var second = randomVector(0, size[0]);
-    var third = randomVector(0, size[0]);
-    var time = new Date().getTime();
-    canvasTriangles.drawTriangle(first, second, third, g);
-    avgTime += (new Date().getTime() - time) / 1000;
-}
-console.log(avgTime / samples);
-
-canvasTriangles.paintImage();
-
-var img = ImageIO.loadImage("R.png");
-
-var i = 0;
-var j = 0;
-var t = 0;
-var a = [-1, 1];
-var v = [0.1, 0.1];
-var n = [1, -1];
-var speed = 0.01;
-var points = giveMeLine(a, v);
-function draw() {
-    canvasLines.drawLine(points[0], points[1], interpolativeShader);
-
-    points[0] = [points[0][0] + speed * n[0], points[0][1] + speed * n[1]];
-    points[1] = [points[1][0] + speed * n[0], points[1][1] + speed * n[1]];
-
-    canvasLines.paintImage();
-
-    canvasPoints.clearImage([0, 0, 0, 255]);
-    canvasPoints.drawPxl([i, j], [255, 0, 0, 255]);
-    canvasPoints.drawPxl([i + 1, j], [255, 0, 0, 255]);
-    canvasPoints.drawPxl([i - 1, j], [255, 0, 0, 255]);
-    canvasPoints.drawPxl([i, j - 1], [255, 0, 0, 255]);
-    canvasPoints.drawPxl([i, j + 1], [255, 0, 0, 255]);
-    if (img.isReady) {
-        canvasPoints.drawImage(img, [i + 10, j]);
-    }
-    canvasPoints.paintImage();
-    t++;
-    var sizePoints = canvasPoints.getSize();
-    i = t % sizePoints[0];
-    j = Math.floor(t / sizePoints[0]);
-    requestAnimationFrame(draw);
-}
-
-requestAnimationFrame(draw);
+//
+//var size = canvasTriangles.getSize();
+//canvasTriangles.drawLine([0, Math.floor(size[0] / 10)], [size[1], Math.floor(size[0] / 10)], r);
+//canvasTriangles.drawLine([Math.floor(size[1] / 10), 0], [Math.floor(size[1] / 10), size[0]], g);
+//canvasTriangles.drawLine([0, 0], [size[0]-1, size[1]-1], f);
+//
+//var avgTime = 0;
+//for(var i = 0; i < samples; i++) {
+//    var first = randomVector(0, size[0]);
+//    var second = randomVector(0, size[0]);
+//    var third = randomVector(0, size[0]);
+//    var time = new Date().getTime();
+//    canvasTriangles.drawTriangle(first, second, third, g);
+//    avgTime += (new Date().getTime() - time) / 1000;
+//}
+//console.log(avgTime / samples);
+//
+//canvasTriangles.paintImage();
+//
+//var img = ImageIO.loadImage("R.png");
+//
+//var i = 0;
+//var j = 0;
+//var t = 0;
+//var a = [-1, 1];
+//var v = [0.1, 0.1];
+//var n = [1, -1];
+//var speed = 0.01;
+//var points = giveMeLine(a, v);
+//function draw() {
+//    canvasLines.drawLine(points[0], points[1], interpolativeShader);
+//
+//    points[0] = [points[0][0] + speed * n[0], points[0][1] + speed * n[1]];
+//    points[1] = [points[1][0] + speed * n[0], points[1][1] + speed * n[1]];
+//
+//    canvasLines.paintImage();
+//
+//    canvasPoints.clearImage([0, 0, 0, 255]);
+//    canvasPoints.drawPxl([i, j], [255, 0, 0, 255]);
+//    canvasPoints.drawPxl([i + 1, j], [255, 0, 0, 255]);
+//    canvasPoints.drawPxl([i - 1, j], [255, 0, 0, 255]);
+//    canvasPoints.drawPxl([i, j - 1], [255, 0, 0, 255]);
+//    canvasPoints.drawPxl([i, j + 1], [255, 0, 0, 255]);
+//    if (img.isReady) {
+//        canvasPoints.drawImage(img, [i + 10, j]);
+//    }
+//    canvasPoints.paintImage();
+//    t++;
+//    var sizePoints = canvasPoints.getSize();
+//    i = t % sizePoints[0];
+//    j = Math.floor(t / sizePoints[0]);
+//    requestAnimationFrame(draw);
+//}
+//
+//requestAnimationFrame(draw);
 
 
 },{"./CanvasSpace.js":1,"./ImageIO.js":2,"./MyCanvas.js":3}]},{},[4]);
