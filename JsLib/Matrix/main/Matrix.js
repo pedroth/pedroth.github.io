abdd19be0cac79a9ac1e53fe5301cbd82f37304a
pedroth <pedrotiago92@gmail.com>
2018-09-05 00:56:19 -0400

    finish discrete calculus blog

--- JsLib/Matrix/main/Matrix.js
+++ JsLib/Matrix/main/Matrix.js
@@ -1,13 +1,7 @@
 var DenseNDArray = require('../DenseNDArray/main/DenseNDArray.js');
 
 var Matrix = function(array) {
-    var map = {}
-    map[DenseNDArray] = () => { 
-                                if(array.shape() > 2) {
-                                    
-                                }
-                                 DenseNDArray.of(array)
-                              }
+    var map = {};
     if(array instanceof DenseNDArray) {
         this.matrix = DenseNDArray.of(array)
     }
