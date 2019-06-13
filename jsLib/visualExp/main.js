b2280846a4f204c19bcbcf1618deb0193dabaf5d
pedroth <pedrotiago92@gmail.com>
2019-06-13 09:03:35 +0100

    minor changes add package.locks add gitignore

--- jsLib/visualExp/main.js
+++ jsLib/visualExp/main.js
@@ -1,8 +1,13 @@
 const VisualExp = {};
 
 VisualExp.retrieveAndAppend = async function(url, htmlId) {
-  const html = await fetch(url).then(x => x.json);
+  const html = await fetch(url).then(x => x.text());
   document.getElementById(htmlId).innerHTML = html;
 };
+
+VisualExp.retrieveAndAppend(
+  "resources/templates/nav/nav.html",
+  "indexContainer"
+);
 
 module.exports.default = VisualExp;
--- jsLib/visualExp/webpack.config.js
+++ jsLib/visualExp/webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 module.exports = {
   entry: "./main.js",
+  mode: "development",
   output: {
     path: path.resolve("./"),
     filename: "index.js",
