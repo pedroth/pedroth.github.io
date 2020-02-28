330e792a2a2a638adc91eefab35a61df22a2f023
pedroth <pedrotiago92@gmail.com>
2020-02-28 23:14:10 +0000

    update search test

--- jslib/src/Pedroth/Pedroth.js
+++ jslib/src/Pedroth/Pedroth.js
@@ -1,5 +1,6 @@
 const DomBuilder = require("../DomBuilder/main/DomBuilder");
 const WebUtils = require("../WebUtils/WebUtils");
 const Card = require("../Card/Card");
+const Nabla = require("nabla.js");
 
-export { DomBuilder, WebUtils, Card };
+export { DomBuilder, WebUtils, Card, Nabla };
--- static/searchBar/searchBar.js
+++ static/searchBar/searchBar.js
@@ -1,20 +1,33 @@
-const { DomBuilder } = Pedroth;
+const { DomBuilder, Nabla } = Pedroth;
+const { ArrayUtils } = Nabla;
 const uuid = key => `${key}${Math.random()}`;
+const range = a => b => (a < b ? [a].concat(range(a + 1)(b)) : []);
+const range0 = range(0);
+const mod = n => i => (n + (i % n)) % n;
+const doIO = list => () => list.forEach(f => f());
 
 class SearchBar {
+  static defaultProps = {
+    onClick: input => {},
+    onChange: (input, searchBar) => {
+      searchBar.setSuggestions(range0(10).map(Math.random));
+    },
+    buttonLabel: "search"
+  };
+
   constructor(props) {
-    this.props = props;
+    this.props = { ...SearchBar.defaultProps, ...props };
     this.id = uuid("id");
-    this.idSuggestion = uuid("id");
     this.idInput = uuid("id");
+    this.idSuggestion = uuid("id");
     this.domComponent = this.buildDOMComponent();
     // not selected
-    this.selectedIndex = -1;
+    this.selectedIndex = null;
     this.inputValue = "";
     this.suggestionList = [];
   }
 
-  getComponent = () => this.domComponent;
+  getDOM = () => this.domComponent;
 
   buildDOMComponent = () =>
     DomBuilder.of("div")
@@ -27,17 +40,56 @@ class SearchBar {
     DomBuilder.of("input")
       .attr("id", this.idInput)
       .event("input", e => {
-        this.setSuggestions(range0(10).map(Math.random));
         this.inputValue = e.target.value;
+        this.props.onChange(this.inputValue, this);
         this.render();
       })
       .event("keydown", evt => {
+        console.log("Key pressed");
         const keyCodeAction = {
-          13: this.props.onClick
+          13: this.props.onClick,
+          38: this.highLightNextSuggestion(-1),
+          40: this.highLightNextSuggestion(1)
         };
         const action = keyCodeAction[evt.keyCode];
         action && action(this.inputValue);
       });
+
+  highLightNextSuggestion = step => () => {
+    if (this.selectedIndex == null) {
+      this.highLightIndex(0);
+    } else {
+      this.highLightIndex(
+        mod(this.suggestionList.length)(this.selectedIndex + step)
+      );
+    }
+  };
+
+  highLightIndex = index => {
+    const domSuggestions = DomBuilder.ofId(this.idSuggestion).build()
+      .children[0].children;
+
+    ArrayUtils.forEach(domSuggestions, dom => (dom.style = ""));
+
+    const domSuggestionIndex = ArrayUtils.filter(
+      domSuggestions,
+      (dom, i) => index === i
+    );
+
+    this.selectedIndex = index;
+    if (index === null) return;
+
+    DomBuilder.ofId(this.idInput).build().value = this.suggestionList[index];
+    this.inputValue = this.suggestionList[index];
+
+    ArrayUtils.forEach(
+      domSuggestionIndex,
+      dom => (dom.style = this.highLightStyle)
+    );
+  };
+
+  highLightStyle =
+    "background-color:rgba(100, 100, 100); color:rgb(255,255,255);";
 
   getButton = () =>
     DomBuilder.of("button")
@@ -45,6 +97,7 @@ class SearchBar {
       .inner(this.props.buttonLabel);
 
   render = () => {
+    console.log("Render", this.selectedIndex);
     this.renderSuggestions();
   };
 
@@ -70,22 +123,22 @@ class SearchBar {
       );
   };
 
-  createCard = suggestion => {
+  createCard = (suggestion, index) => {
     return DomBuilder.of("li")
-      .attr("style", "background-color: 'white', color: 'black'")
+      .attr("style", index === this.selectedIndex ? this.highLightStyle : "")
       .attr("class", "list-group-item")
       .event("click", () => {
         DomBuilder.ofId(this.idInput).build().value = suggestion;
         this.inputValue = suggestion;
         DomBuilder.ofId(this.idSuggestion).removeChildren();
       })
-      .event(
-        "mouseover",
-        evt =>
-          (evt.target.style =
-            "background-color:rgba(100, 100, 100); color:rgb(255,255,255);")
-      )
-      .event("mouseout", evt => (evt.target.style = ""))
+      .event("mouseover", evt => {
+        console.log("Mouse over", index);
+        this.highLightIndex(index);
+      })
+      .event("mouseout", evt => {
+        this.highLightIndex(null);
+      })
       .html(suggestion);
   };
 
@@ -101,7 +154,4 @@ const searchComponent = new SearchBar({
   onClick: searchInput => (window.location.href = `?q=${searchInput}`),
   buttonLabel: "search"
 });
-DomBuilder.ofId("root").append(searchComponent.getComponent());
-
-const range = a => b => (a < b ? [a].concat(range(a + 1)(b)) : []);
-const range0 = range(0);
+DomBuilder.ofId("root").append(searchComponent.getDOM());
