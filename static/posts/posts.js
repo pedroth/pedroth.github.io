8c877e14d83c28db4dc59a68895c3245bbea3999
cristovao <pedro.cristovao@mov.ai>
2021-02-08 22:56:35 +0000

    - create grid of cards without bootstrap - add badge bootstrap style to index.css

--- static/posts/posts.js
+++ static/posts/posts.js
@@ -1,9 +1,36 @@
-const getCardGrid = posts => {
-  const row = DomBuilder.of("div").attr("class", "row");
+//========================================================================================
+/*                                                                                      *
+ *                                      CARDS GRID                                      *
+ *                                                                                      */
+//========================================================================================
+
+function getRowReSize(rowDiv) {
+  return () => {
+    if (window.innerWidth >= window.innerHeight) {
+      const n = rowDiv.childNodes.length;
+      const w = window.innerWidth / n;
+      rowDiv.style["flex-direction"] = "row";
+      rowDiv.childNodes.forEach(child => {
+        child.style.width = `${w}px`;
+        child.style["max-width"] = `100%`;
+      });
+    } else {
+      rowDiv.style["flex-direction"] = "column";
+      rowDiv.style["align-items"] = "center";
+      const w = window.innerWidth * 0.77;
+      rowDiv.childNodes.forEach(child => (child.style.width = `${w}px`));
+    }
+  };
+}
+
+function getCardsInRow(posts) {
+  const row = DomBuilder.of("div").attr(
+    "style",
+    "display: flex; max-width: 100%"
+  );
   posts.forEach(post => {
     const col = DomBuilder.of("div")
-      .attr("class", "col-lg-4 col-md-6 col-sm-12")
-      .attr("style", "margin-top:10px; margin-bottom: 10px;")
+      .attr("style", "margin: 10px;")
       .append(
         Card.builder()
           .imageSrc(`${post.src}/${post.id}.gif`)
@@ -16,10 +43,30 @@ const getCardGrid = posts => {
       .build();
     row.append(col);
   });
-  return row.build();
+  const rowDiv = row.build();
+  const rowResize = getRowReSize(rowDiv);
+  // from index.js
+  window.sizeObservers.push(rowResize);
+  rowResize();
+  return rowDiv;
+}
+
+const getCardGrid = (posts, k = 3) => {
+  const body = DomBuilder.of("div");
+  const n = Math.floor(posts.length / k);
+  for (let i = 0; i < n; i++) {
+    const row = getCardsInRow(posts.splice(i * k, k), k);
+    body.append(row);
+  }
+  return body.build();
 };
 
-// main
+//========================================================================================
+/*                                                                                      *
+ *                                         MAIN                                         *
+ *                                                                                      */
+//========================================================================================
+
 (() => {
   WebUtils.readDb().then(db => {
     DomBuilder.ofId("results").append(
--- static/search/search.js
+++ static/search/search.js
@@ -1,27 +1,8 @@
-const getCardGrid = posts => {
-  const row = DomBuilder.of("div").attr("class", "row");
-  posts.forEach(post => {
-    const col = DomBuilder.of("div")
-      .attr("class", "col-lg-4 col-md-6 col-sm-12")
-      .attr("style", "margin-top:10px; margin-bottom: 10px;")
-      .append(
-        Card.builder()
-          .imageSrc(`${post.src}/${post.id}.gif`)
-          .url(`/?p=${post.src}/${post.id}.html`)
-          .title(post.title)
-          .tags(post.tags)
-          .date(post.date)
-          .build()
-      )
-      .build();
-    row.append(col);
-  });
-  const rowDiv = row.build();
-  const rowResize = getRowReSize(rowDiv);
-  window.sizeObservers.push(rowResize);
-  rowResize();
-  return rowDiv;
-};
+//========================================================================================
+/*                                                                                      *
+ *                                      CARDS GRID                                      *
+ *                                                                                      */
+//========================================================================================
 
 function getRowReSize(rowDiv) {
   return () => {
@@ -42,7 +23,50 @@ function getRowReSize(rowDiv) {
   };
 }
 
-// main
+function getCardsInRow(posts) {
+  const row = DomBuilder.of("div").attr(
+    "style",
+    "display: flex; max-width: 100%"
+  );
+  posts.forEach(post => {
+    const col = DomBuilder.of("div")
+      .attr("style", "margin: 10px;")
+      .append(
+        Card.builder()
+          .imageSrc(`${post.src}/${post.id}.gif`)
+          .url(`/?p=${post.src}/${post.id}.html`)
+          .title(post.title)
+          .tags(post.tags)
+          .date(post.date)
+          .build()
+      )
+      .build();
+    row.append(col);
+  });
+  const rowDiv = row.build();
+  const rowResize = getRowReSize(rowDiv);
+  // from index.js
+  window.sizeObservers.push(rowResize);
+  rowResize();
+  return rowDiv;
+}
+
+const getCardGrid = (posts, k = 3) => {
+  const body = DomBuilder.of("div");
+  const n = Math.floor(posts.length / k);
+  for (let i = 0; i < n; i++) {
+    const row = getCardsInRow(posts.splice(i * k, k), k);
+    body.append(row);
+  }
+  return body.build();
+};
+
+//========================================================================================
+/*                                                                                      *
+ *                                         MAIN                                         *
+ *                                                                                      */
+//========================================================================================
+
 (() => {
   let query = decodeURIComponent(
     window.location.href.split("?")[1].split("=")[1]
