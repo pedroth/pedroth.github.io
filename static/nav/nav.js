f14ad46da7b0671d778facf0e2ca140512e69ed9
pedroth <pedrotiago92@gmail.com>
2021-12-04 19:08:31 +0000

    fix search engine

--- static/nav/nav.js
+++ static/nav/nav.js
@@ -41,75 +41,14 @@ function getRecommendations(query, searchBar) {
     searchBar.setSuggestions([]);
     return;
   }
-  const titleSuggestions = getTitleSuggestions(query);
-  const tagSuggestions = getTagSuggestions(query);
-  searchBar.setSuggestions([...titleSuggestions, ...tagSuggestions]);
-}
-
-function getTitleSuggestions(query) {
-  const { distance } = Nabla.EditDistance;
-  const queryLength = len(query);
-  const finalDist = (q, str) => distance(q, subStr(str, queryLength));
-  const titleFeaturesObjList = titles
-    .map(title => title.toLowerCase())
-    .map(title => {
-      const titleKws = title.split(" ");
-      return {
-        title: title,
-        distance: finalDist(query, title),
-        minDistanceKw: titleKws.reduce(
-          (min, tkw) => Math.min(min, finalDist(query, tkw)),
-          Number.MAX_VALUE
-        ),
-        wordsRationInAcceptableDistance:
-          titleKws.filter(titleKw => finalDist(query, titleKw) < 3).length /
-          titleKws.length
-      };
-    });
-  const suggestions = titleFeaturesObjList
-    .map(
-      ({
-        title,
-        distance,
-        minDistanceKw,
-        wordsRationInAcceptableDistance
-      }) => ({
-        title,
-        distance: distance + minDistanceKw - wordsRationInAcceptableDistance
-      })
-    )
-    .sort((a, b) => a.distance - b.distance)
-    .map(({ title }) => title);
-  return suggestions.splice(0, 5);
-}
-
-function getTagSuggestions(query) {
-  return [];
-  // const { distance: d } = Nabla.EditDistance;
-  // const qSplit = query.split("+").map(s => s.trim());
-  // const finalQuery = len(qSplit) > 1 ? qSplit[len(qSplit) - 1] : qSplit[0];
-  // const tags = Object.keys(tagsHist);
-  // const sortedTags = tags
-  //   .map(t => ({
-  //     name: t,
-  //     distance:
-  //       (d(finalQuery, subStr(t, len(finalQuery))) + d(finalQuery, t)) / 2
-  //   }))
-  //   .sort((a, b) => a.distance - b.distance);
-  // const suggestions = sortedTags
-  //   .filter(t => t.distance <= 7)
-  //   .filter((t, i) => i < 7)
-  //   .map(z => z.name);
-  // return suggestions;
-}
-
-function onSetSuggestion(prevValue, suggestion) {
-  const split = prevValue.split("+").map(z => z.trim());
-  if (split.length > 1) {
-    split[split.length - 1] = suggestion;
-    return split.join(" + ");
-  }
-  return suggestion;
+  const queryT = query.toLowerCase().trim();
+  const tags = Object.keys(tagsHist);
+  const { distance: editDistance } = Nabla.EditDistance;
+  const suggestions = [...titles, ...tags]
+    .map(s => s.toLowerCase())
+    .filter(s => s.includes(queryT))
+    .sort((a, b) => editDistance(a, queryT) - editDistance(b, queryT));
+  searchBar.setSuggestions(suggestions.splice(0, 5))
 }
 
 //========================================================================================
@@ -121,7 +60,6 @@ function onSetSuggestion(prevValue, suggestion) {
 const searchComponent = new SearchInput({
   onClick: searchInput => (window.location.href = `?q=${searchInput}`),
   onChange: getRecommendations,
-  onSetInput: onSetSuggestion,
   buttonDom: DomBuilder.of("button")
     .attr("class", "navIcons")
     .append(DomBuilder.of("i").attr("class", "fas fa-search"))
@@ -129,7 +67,7 @@ const searchComponent = new SearchInput({
   inputDom: DomBuilder.of("input")
     .attr("class", "searchClass")
     .attr("type", "text")
-    .attr("placeholder", "Search... use + to concat")
+    .attr("placeholder", "Search...")
     .build(),
   highLightStyle:
     "background-color:rgba(255, 255, 255); color:rgb(100, 100, 100)",
